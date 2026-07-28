84aa03f2fece5dc569b4412bb7a5adcdf3a44ba8
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2026-07-29 02:08:05 +0530

    Time: 25 ms (76.14%), Space: 80.8 MB (11.28%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-
         vector<int> freq(26, 0);
-
-        for (char c : s)
+        for (char c : s) {
             freq[c - 'a']++;
-
+        }
+        
         string left = "";
         string middle = "";
-
-        for (int i = 0; i < 26; i++) {
-
-            left.append(freq[i] / 2, char('a' + i));
-
-            if (freq[i] % 2)
-                middle = char('a' + i);
+        
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] % 2 != 0) {
+                middle = string(1, (char)('a' + i));
+            }
+            left += string(freq[i] / 2, (char)('a' + i));
         }
-
+        
         string right = left;
         reverse(right.begin(), right.end());
-
+        
         return left + middle + right;
     }
 };
