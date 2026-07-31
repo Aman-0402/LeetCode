63f9fd92ad0c9fa574c9c2ba1f2085f3b6ed4070
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2026-07-31 11:12:52 +0530

    Time: 12 ms (55.56%), Space: 13.7 MB (55.56%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int *)b - *(int *)a);
+}
+
+int minimumPushes(char* word) {
+    int freq[26] = {0};
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        freq[word[i] - 'a']++;
+    }
+
+    qsort(freq, 26, sizeof(int), compare);
+
+    int ans = 0;
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] == 0)
+            break;
+
+        ans += freq[i] * (i / 8 + 1);
+    }
+
+    return ans;
+}
