c3ae124316a926534d6b99500771436dad536a02
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2026-08-05 09:47:47 +0530

    Time: 244 ms (84.09%), Space: 146 MB (29.54%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+
+        graph = [[] for _ in range(n)]
+
+        for u, v in invocations:
+            graph[u].append(v)
+
+        vis = [False] * n
+
+        def dfs(u):
+            vis[u] = True
+
+            for v in graph[u]:
+                if not vis[v]:
+                    dfs(v)
+
+        dfs(k)
+
+        for u, v in invocations:
+            if not vis[u] and vis[v]:
+                return list(range(n))
+
+        return [i for i in range(n) if not vis[i]]
