7fb6133a8e3044e59d3d8006c05f07a451831bcc
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2026-08-10 09:14:06 +0530

    Time: 19 ms (72.73%), Space: 9.3 MB (18.18%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.c
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool winnerSquareGame(int n) {
+    bool* dp = (bool*)calloc(n + 1, sizeof(bool));
+
+    for (int i = 1; i <= n; ++i) {
+
+        for (int j = 1; j * j <= i; ++j) {
+            int square = j * j;
+
+            if (!dp[i - square]) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    bool result = dp[n];
+
+    free(dp);
+
+    return result;
+}
