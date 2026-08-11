72b59fbb559d1288096ec7c836014c3169e103cb
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2026-08-11 09:10:53 +0530

    Time: 0 ms (100%), Space: 55.9 MB (21.74%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingInteger = function(nums) {
+    let sum = nums[0];
+
+    let i = 1;
+
+    while (
+        i < nums.length &&
+        nums[i] === nums[i - 1] + 1
+    ) {
+        sum += nums[i];
+        ++i;
+    }
+
+    const seen = new Set(nums);
+
+    while (seen.has(sum)) {
+        ++sum;
+    }
+
+    return sum;
+};
