1451fbe9304a636a7f7e2bfcea7829d4b0a83f6f
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2026-08-24 07:37:56 +0530

    Time: 82 ms (11.11%), Space: 66.6 MB (88.89%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var stoneGameVIII = function(stones) {
+    const n = stones.length;
+
+    const prefix = new Array(n);
+    prefix[0] = stones[0];
+
+    for (let i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + stones[i];
+    }
+
+    let dp = prefix[n - 1];
+
+    for (let i = n - 2; i >= 1; i--) {
+        dp = Math.max(dp, prefix[i] - dp);
+    }
+
+    return dp;
+};
