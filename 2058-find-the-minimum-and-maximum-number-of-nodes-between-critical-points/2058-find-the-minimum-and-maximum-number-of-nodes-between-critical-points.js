e7d9e7154ad2bd0b1c5878130cddd70e8237ede6
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2026-08-31 22:19:24 +0530

    Time: 3 ms (98.08%), Space: 84.3 MB (94.23%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var nodesBetweenCriticalPoints = function(head) {
+
+    let first = -1;
+    let last = -1;
+    let prevCritical = -1;
+
+    let minDist = Infinity;
+
+    let prev = head;
+    let curr = head.next;
+
+    let index = 1;
+
+    while (curr.next !== null) {
+
+        const critical =
+            (curr.val > prev.val &&
+             curr.val > curr.next.val) ||
+
+            (curr.val < prev.val &&
+             curr.val < curr.next.val);
+
+        if (critical) {
+
+            if (first === -1) {
+                first = index;
+            }
+
+            if (prevCritical !== -1) {
+                minDist = Math.min(
+                    minDist,
+                    index - prevCritical
+                );
+            }
+
+            prevCritical = index;
+            last = index;
+        }
+
+        prev = curr;
+        curr = curr.next;
+        index++;
+    }
+
+    if (first === last) {
+        return [-1, -1];
+    }
+
+    const maxDist = last - first;
+
+    return [minDist, maxDist];
+};
